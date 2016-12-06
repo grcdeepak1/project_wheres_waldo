448745ef60e45e7fb5caf1a35d85f32e7e3943ca
Deepak <grc.deepak@gmail.com>
2016-12-06 12:34:25 -0800

    delete logic

--- app/assets/javascripts/tag_model.js
+++ app/assets/javascripts/tag_model.js
@@ -4,6 +4,7 @@ TAG.Model = (function() {
 
   var _charList = ['Waldo', 'Wenda', 'Odlaw', 'Whizard Whitebeard', 'Woof'];
   var _tags     = [];
+  var _gameStartTime = undefined;
 
   var addTag = function(tagObject) {
     _tags.push(tagObject);
@@ -12,6 +13,19 @@ TAG.Model = (function() {
   var getCharList = function() {
     return _charList;
   }
+
+  var removeCharFromList = function(character) {
+    _charList.forEach( function(c, i) {
+      if(c === character) {
+        _charList.splice(i, 1);
+      }
+    })
+  }
+
+  var addCharToList = function(character) {
+    _charList.push(character);
+  }
+
   var tags = function() {
     return _tags;
   }
@@ -19,9 +33,26 @@ TAG.Model = (function() {
   var removeLastTag = function() {
     _tags.pop();
   }
+
+  var setGameStartTime = function() {
+    _gameStartTime = $.now();
+  }
+
+  var gameStartTime = function() {
+    return _gameStartTime;
+  }
+
+  var init = function() {
+    setGameStartTime();
+  }
+
   return {
+    init: init,
     getCharList: getCharList,
     tags: tags,
     removeLastTag: removeLastTag,
+    gameStartTime: gameStartTime,
+    removeCharFromList: removeCharFromList,
+    addCharToList: addCharToList,
   }
 })();
